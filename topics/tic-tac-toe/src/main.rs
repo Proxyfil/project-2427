6ddbf553c71f5c9a6a51936078d094c1f93abe9a
Proxyfil <piloupilou200@gmail.com>
2025-09-28 17:21:44 +0200

    feat(src): Created source code to create board and game objects

--- topics/tic-tac-toe/src/main.rs
+++ topics/tic-tac-toe/src/main.rs
@@ -1,3 +1,10 @@
+mod game;
+
+use crate::game::game::Game;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting Tic Tac Toe game..." );
+    let mut game = Game::new();
+
+    game.play();
 }
